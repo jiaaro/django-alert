c59e6a1b52688b99110243fa02979e10ce687986
James Robert <jiaaro@gmail.com>
2011-05-26 17:06:10 -0400

    added bulk update actions to admin

--- alert/admin.py
+++ alert/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
 from models import Alert, AlertPreference
 
+
+
 class AlertAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'backend', 'alert_type', 'failed', 'is_sent', 'created',)
     list_filter = ('alert_type', 'backend', 'is_sent', 'failed')
     search_fields = ('=user__username', '=user__email')
 
+
 class AlertPrefAdmin(admin.ModelAdmin):
     list_display = ("user", 'alert_type', "backend", 'preference')
     list_filter = ('alert_type', 'backend', 'preference')
     search_fields = ('=user__username', '=user__email')
+    actions = ['subscribe', 'unsubscribe']
+    
+    def unsubscribe(self, request, qs): 
+        qs.update(preference=False)
+    unsubscribe.short_description = 'Set selected preferences to "Unsubscribed"'
+    
+    def subscribe(self, request, qs): 
+        qs.update(preference=True)
+    subscribe.short_description = 'Set selected preferences to "Subscribed"'
+
     
 
 admin.site.register(Alert, AlertAdmin)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='django-alert',
-    version='0.1.7',
+    version='0.1.8',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description=('Send alerts, notifications, and messages based '
