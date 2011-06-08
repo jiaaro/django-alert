28626848b535e40ff550fa919ac1a2253cc71b93
James Robert <jiaaro@gmail.com>
2011-06-08 01:00:21 -0400

    fix html emails in the email backend

--- alert/backends.py
+++ alert/backends.py
@@ -1,9 +1,11 @@
-from datetime import datetime
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 
 from alert.utils import BaseAlertBackend
 from alert.exceptions import CouldNotSendError
+from django.template.defaultfilters import striptags
+
 
 class EmailBackend(BaseAlertBackend):
     title = "Email"
@@ -12,6 +14,19 @@ class EmailBackend(BaseAlertBackend):
         recipient = alert.user.email
         if not recipient: raise CouldNotSendError
         
-        send_mail(alert.title, alert.body, settings.DEFAULT_FROM_EMAIL, [recipient])
+        subject = alert.title
+        to = [recipient]
+        from_email = settings.DEFAULT_FROM_EMAIL 
         
-    
+        if alert.alert_type_obj.template_filetype == 'html':
+            html_content = alert.body
+            text_content = striptags(html_content)
+            
+            msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+            
+        else:
+            send_mail(subject, alert.body, from_email, to)
+        
+        
