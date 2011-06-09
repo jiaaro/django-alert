c08a26f8ff38bbb7fe09144c2ecd6e7ee9bb65d6
James Robert <jiaaro@gmail.com>
2011-06-09 11:40:22 -0400

    increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='django-alert',
-    version='0.4.4',
+    version='0.4.5',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description=('Send alerts, notifications, and messages based '
