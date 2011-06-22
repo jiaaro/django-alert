c0b0551871e923d79adc345b9ee905712311ee9f
James Robert <jiaaro@gmail.com>
2011-06-22 00:06:30 -0400

    increment minor version for new admin mailing feature

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='django-alert',
-    version='0.5.2',
+    version='0.6.0',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description=('Send alerts, notifications, and messages based '
